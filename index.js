720281f2bc3dfebcd394cfbba4c11b3e61b5434b
BashirAbdusslam <bashirabdusslam34@gmail.com>
2026-07-31 11:35:49 -0700

    Switch to NIGFILMBOT

--- index.js
+++ index.js
@@ -432,7 +432,7 @@ bot.action(/^admin_publish_film_(\d+)$/, async (ctx) => {
         [
           Markup.button.url(
             "💳 BUY NOW",
-            `https://t.me/El_bash_movie_bot?start=film_${film.id}`
+            `https://t.me/Nigfilm_bot?start=film_${film.id}`
           ),
         ],
       ]),
